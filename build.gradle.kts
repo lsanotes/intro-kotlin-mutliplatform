8183b2bab451b735e27b2a518055c82ba046a2fd
Liu Pei Pei <lsanotes@live.com>
2019-12-25 11:53:39 +0800

    update ktor
    ktor ~ 1.3.0-rc
    
    (cherry picked from commit 54d4feffd2844ec53721f0867f61108b349e2fc4)
    (cherry picked from commit 422cdcbc46d0199cb29eb1d6f99f0d3cf4651d7e)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  kotlin("multiplatform") version "1.3.40"
+  kotlin("multiplatform") version "1.3.61"
 }
 
 kotlin {
@@ -14,7 +14,7 @@ repositories {
   mavenCentral()
 }
 
-val ktorVersion = "1.2.2"
+val ktorVersion = "1.3.0-rc"
 val logbackVersion = "1.2.3"
 
 kotlin.sourceSets["jsMain"].dependencies {
